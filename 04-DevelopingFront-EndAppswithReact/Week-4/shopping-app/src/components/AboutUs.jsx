262d22e8e4986a941dab1dcd0d87c447c857cd97
Rohit46432567788 <153052221+Rohit46432567788@users.noreply.github.com>
2026-04-20 23:44:06 +0530

    Create AboutUs.jsx

---
+++ 04-DevelopingFront-EndAppswithReact/Week-4/shopping-app/src/components/AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Paradise Nursery 🌿</h1>
+      <p>
+        Paradise Nursery is an online plant shopping application where users can
+        explore different types of plants and add them to their cart.
+      </p>
+      <p>
+        Our mission is to bring nature closer to people by providing high-quality
+        plants and a seamless shopping experience.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
