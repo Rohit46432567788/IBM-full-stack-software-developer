d4035026efeb1415a9498246126fc2fd9ff9097a
Rohit46432567788 <153052221+Rohit46432567788@users.noreply.github.com>
2026-04-20 23:50:01 +0530

    Update App.js

--- 04-DevelopingFront-EndAppswithReact/Week-4/shopping-app/src/App.js
+++ 04-DevelopingFront-EndAppswithReact/Week-4/shopping-app/src/App.js
@@ -3,17 +3,35 @@ import "./App.css";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", paddingTop: "100px" }}>
-      <h1>🌿 Paradise Nursery</h1>
-      <p>Welcome to your one-stop plant shop</p>
+    <div
+      style={{
+        textAlign: "center",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ fontSize: "3rem", marginBottom: "10px" }}>
+        🌿 Paradise Nursery
+      </h1>
+
+      <p style={{ fontSize: "1.2rem", marginBottom: "20px" }}>
+        Welcome to your one-stop destination for beautiful plants
+      </p>
 
       <button
         style={{
-          padding: "10px 20px",
+          padding: "12px 25px",
           fontSize: "16px",
+          border: "none",
+          borderRadius: "5px",
+          backgroundColor: "green",
+          color: "white",
           cursor: "pointer",
         }}
-        onClick={() => alert("Get Started")}
+        onClick={() => alert("Get Started clicked!")}
       >
         Get Started
       </button>
