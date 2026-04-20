3b10a904226ce97e728e8017fdf52dfb699e4558
Rohit46432567788 <153052221+Rohit46432567788@users.noreply.github.com>
2026-04-20 23:53:04 +0530

    Update App.js

--- 04-DevelopingFront-EndAppswithReact/Week-4/shopping-app/src/App.js
+++ 04-DevelopingFront-EndAppswithReact/Week-4/shopping-app/src/App.js
@@ -1,46 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [started, setStarted] = useState(false);
-
-  if (!started) {
-    return (
-      <div
-        style={{
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-        }}
-      >
-        <h1>🌿 Paradise Nursery</h1>
-        <p>Welcome to your one-stop plant shop</p>
-
-        <button
-          onClick={() => setStarted(true)}
-          style={{
-            padding: "12px 25px",
-            fontSize: "16px",
-            backgroundColor: "green",
-            color: "white",
-            border: "none",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}
-        >
-          Get Started
-        </button>
-      </div>
-    );
-  }
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h2>🌱 Browse Plants</h2>
-      <p>Product list will appear here</p>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Paradise Nursery</h1>
+      <p>Welcome to our plant shop</p>
+
+      <button>Get Started</button>
     </div>
   );
 }
