d19246624cef5e761ca5b9f28e893909ddc68689
Rohit46432567788 <153052221+Rohit46432567788@users.noreply.github.com>
2026-04-20 23:51:52 +0530

    Update App.js

--- 04-DevelopingFront-EndAppswithReact/Week-4/shopping-app/src/App.js
+++ 04-DevelopingFront-EndAppswithReact/Week-4/shopping-app/src/App.js
@@ -1,44 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-const App = () => {
-  const handleClick = () => {
-    alert("Welcome to Paradise Nursery!");
-  };
+function App() {
+  const [started, setStarted] = useState(false);
 
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        textAlign: "center",
-      }}
-    >
-      <h1>🌿 Paradise Nursery</h1>
-
-      <p>
-        Discover a wide variety of indoor and outdoor plants for your home.
-      </p>
-
-      <button
-        onClick={handleClick}
+  if (!started) {
+    return (
+      <div
         style={{
-          padding: "10px 20px",
-          fontSize: "16px",
-          backgroundColor: "green",
-          color: "white",
-          border: "none",
-          borderRadius: "5px",
-          cursor: "pointer",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
         }}
       >
-        Get Started
-      </button>
+        <h1>🌿 Paradise Nursery</h1>
+        <p>Welcome to your one-stop plant shop</p>
+
+        <button
+          onClick={() => setStarted(true)}
+          style={{
+            padding: "12px 25px",
+            fontSize: "16px",
+            backgroundColor: "green",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Get Started
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>🌱 Browse Plants</h2>
+      <p>Product list will appear here</p>
     </div>
   );
-};
+}
 
 export default App;
