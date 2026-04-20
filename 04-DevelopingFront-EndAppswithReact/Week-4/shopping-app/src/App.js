41fc6355290137d398b746b01d365400329d622f
Rohit46432567788 <153052221+Rohit46432567788@users.noreply.github.com>
2026-04-20 23:51:16 +0530

    Update App.js

--- 04-DevelopingFront-EndAppswithReact/Week-4/shopping-app/src/App.js
+++ 04-DevelopingFront-EndAppswithReact/Week-4/shopping-app/src/App.js
@@ -1,42 +1,44 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const handleClick = () => {
+    alert("Welcome to Paradise Nursery!");
+  };
+
   return (
     <div
       style={{
-        textAlign: "center",
         height: "100vh",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        textAlign: "center",
       }}
     >
-      <h1 style={{ fontSize: "3rem", marginBottom: "10px" }}>
-        🌿 Paradise Nursery
-      </h1>
+      <h1>🌿 Paradise Nursery</h1>
 
-      <p style={{ fontSize: "1.2rem", marginBottom: "20px" }}>
-        Welcome to your one-stop destination for beautiful plants
+      <p>
+        Discover a wide variety of indoor and outdoor plants for your home.
       </p>
 
       <button
+        onClick={handleClick}
         style={{
-          padding: "12px 25px",
+          padding: "10px 20px",
           fontSize: "16px",
-          border: "none",
-          borderRadius: "5px",
           backgroundColor: "green",
           color: "white",
+          border: "none",
+          borderRadius: "5px",
           cursor: "pointer",
         }}
-        onClick={() => alert("Get Started clicked!")}
       >
         Get Started
       </button>
     </div>
   );
-}
+};
 
 export default App;
